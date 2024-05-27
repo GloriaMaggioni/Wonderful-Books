0a4820692f8328327d4fdd6d63e1cd7f89568cfa
Gloria Maggioni <glomaggio96@outlook.it>
2024-05-27 18:17:23 +0200

    Inizio creazione del titolo della Home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // file  entry point del progetto
 import '../css/style.css';
+
+
+
+// crearela HOME PAGE
+
+
+const homePageTitle = document.createElement('h1');
+homePageTitle.textContent = "Wonderful Books";
+homePageTitle.style.fontFamily = "Iknut Antiqua";
+homePageTitle.style.fontWeight= 'regular';
+homePageTitle.style.fontSize = '50px';
+homePageTitle.style.textAlign = 'center';
+document.body.appendChild(homePageTitle);
