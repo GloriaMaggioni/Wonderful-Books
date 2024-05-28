c2889f92be6b134e1d8e98879c9a4ce0779b1617
Gloria Maggioni <glomaggio96@outlook.it>
2024-05-28 10:49:54 +0200

    Creazione titolo home page non definitivo

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,6 @@ import '../css/style.css';
 // creare la HOME PAGE
 
 
-// const homePageTitle = document.createElement('h1');
-// homePageTitle.textContent = "Wonderful Books";
-// homePageTitle.style.fontFamily = "Iknut Antiqua";
-// homePageTitle.style.fontWeight= 'regular';
-// homePageTitle.style.fontSize = '50px';
-// homePageTitle.style.textAlign = 'center';
-// document.body.appendChild(homePageTitle);
-
 const createDomElement = (tag, classes, content) =>{
     const el = document.createElement(tag);
     classes.forEach( e => el.classList.add(e));
@@ -22,5 +14,20 @@ const createDomElement = (tag, classes, content) =>{
 
 }
 
-const homePageTitle = createDomElement('h1', ['title'], 'Wonderful Books');
+// let homePageTitle = createTitle();
+
+
+// function createTitle() {
+//     let homePageTitle = createDomElement('h1', ['homePage-title'], 'Wonderful Books');
+//     homePageTitle.style.fontFamily = "Iknut Antiqua";
+//     homePageTitle.style.fontWeight = 'regular';
+//     homePageTitle.style.fontSize = '30px';
+//     homePageTitle.style.textAlign = 'center';
+//     // homePageTitle.style.color = 'red';
+//     return homePageTitle;
+// }
+// document.body.appendChild(homePageTitle);
+
+
+const homePageTitle = createDomElement('h1', ['homePage-title'], 'Wonderful Books');
 document.body.appendChild(homePageTitle);
