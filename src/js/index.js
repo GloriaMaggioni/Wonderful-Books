e806a53acfce745f5657d7b7365e192ebec502ab
Gloria Maggioni <glomaggio96@outlook.it>
2024-09-13 16:37:46 +0200

    Aggiunta chiamata autori funzionante

--- src/js/index.js
+++ src/js/index.js
@@ -55,13 +55,13 @@ async function getBooks(){
       const books = data.docs.map(book =>({
           cover: book.cover_i ? `https://covers.openLibrary.org/b/id/${book.cover_i}-M.jpg` : null,
           title: book.title ? book.title : null,
-
+          author: book.author_name ? book.author_name : null,
           // richiamo autori
 
-      })).filter(book => book.cover && book.title);
+      })).filter(book => book.cover && book.title && book.author);
 
        books.forEach(book =>{
-        const card = createCard()
+        const card = createCard();
 
 
         // covers' books
@@ -70,12 +70,19 @@ async function getBooks(){
           card.appendChild(coverBooksImg);
 
           
-        // book's title
+        // title and author container
+        
+         // book's title
         const titleBook = createDomElement('h3',['title-book'], '');
         titleBook.textContent = book.title;
-        card.appendChild(titleBook)
+        card.appendChild(titleBook);
 
-      
+        // books' author
+        const authorBook = createDomElement('p', ['author-book'], '');
+        authorBook.textContent = book.author;
+        
+      card.appendChild(authorBook);
+
        })
 
       
@@ -96,86 +103,6 @@ homePageButton.addEventListener('click', () =>{
 
 
 
-
-
-
-
-
-
-// async function getBooks() {
-//   let searchBooks = homePageSearch.value;
-//     try{
-//         const response = await axios.get(`https://openLibrary.org/search.json?q=${searchBooks}`);
-//        const data =  response.data;
-//       //  cover books call
-
-
-
-//     //   const coverBooksUrl =   data.docs.map(book => book.cover_i ? `https://covers.openLibrary.org/b/id/${book.cover_i}-M.jpg` : null).filter(url => url);
-//     //   const titleBooksUrl = data.docs.map(book => book.title ? book.title : null).filter(title => title);
-//     //  const books = data.docs.map([coverBooksUrl, titleBooksUrl]);
-//     //  books.forEach( book =>{
-//     //   const coverBooksImg = createDomElement('img', ['coverBook-img'], '');
-//     //     coverBooksImg.src = url;
-//     //    createCard().appendChild(coverBooksImg);
-          
-//     //    const titleBook = createDomElement('h3', ['title-book'], '');
-//     //    titleBook.textContent = title;
-//     //      createCard().appendChild(titleBook);
-//     //  })
-//     //   coverBooksUrl.forEach(url =>{
-//     //     const coverBooksImg = createDomElement('img', ['coverBook-img'], '');
-//     //     coverBooksImg.src = url;
-//     //     createCard().appendChild(coverBooksImg);
-        
-      
-//     //    })      
-
-//     //   //  title book call
-
-//     //   titleBooksUrl.forEach(title =>{
-//     //     const titleBook = createDomElement('h3', ['title-book'], '');
-//     //     titleBook.textContent = title;
-//     //     createCard().appendChild(titleBook);
-//     //   })
-        
-//       // FUNZIONANO ENTRAMBI , PERO BISOGNA CHE IL TITOLO SI ATTACCHI ALLA IMMAGINE CORRISPONDENTE
-     
-//     }catch(error){
-//         console.log(error)
-//     }
-    
-// }
-
-
-// async function getBooks() {
-//   let searchBooks = homePageSearch.value;
-//   try {
-//     const response = await axios.get(`https://openLibrary.org/search.json?q=${searchBooks}`);
-//     const data = response.data;
-
-//     // Crea un array di oggetti che contiene sia il titolo che la copertina di ogni libro
-//     const books = data.docs.map(book => ({
-//       title: book.title ? book.title : null,
-//       cover: book.cover_i ? `https://covers.openLibrary.org/b/id/${book.cover_i}-M.jpg` : null
-//     })).filter(book => book.title && book.cover);
-
-//     // Itera sull'array di libri e crea la carta per ogni libro
-//     books.forEach(book => {
-//       const card = createCard();
-//       const titleBook = createDomElement('h3', ['title-book'], '');
-//       titleBook.textContent = book.title;
-//       card.appendChild(titleBook);
-
-//       const coverBooksImg = createDomElement('img', ['coverBook-img'], '');
-//       coverBooksImg.src = book.cover;
-//       card.appendChild(coverBooksImg);
-//     });
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }     
-//QUESTO FUNZIONA(SCRITTO DAL AI)
 
 
 
