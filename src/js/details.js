10df1c701bdba0d1c948c68c35e4b7af727f4459
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2024-10-13 21:52:52 +0200

    Update details.js

--- src/js/details.js
+++ src/js/details.js
@@ -1,10 +1,40 @@
 import '../css/style.css';
 
-export const detailsContainer = createDomElement('div');
+//book's details container
+export const detailsContainer = document.createElement('div');
 detailsContainer.className = 'details-container';
-detailsContainer.textContent = 'DETAILS PAGEEEESSSSSS'
+detailsContainer.textContent = 'PAGINA DEI DETTAGLI DEI LIBRI';
 
-const bigCoverBook =  document.createElement('div');
+// book's cover
+export const bigCoverBook = document.createElement('div');
 bigCoverBook.className = 'big-cover-book';
 detailsContainer.appendChild(bigCoverBook);
-// NON SI AGGIUNGE LO STILE
+
+
+export const coverImage = document.createElement('img');
+coverImage.placeholder = "image of the book's cover"; 
+bigCoverBook.appendChild(coverImage);
+
+
+//book's title
+export const bookTitle = document.createElement('h2');
+bookTitle.className = 'details-book-title';
+detailsContainer.appendChild(bookTitle);
+
+
+//book's author
+export const detailsBookAuthors = document.createElement('p');
+detailsBookAuthors.className = 'details-book-authors';
+detailsContainer.appendChild(detailsBookAuthors);
+
+//book's description
+
+export const bookDescription = document.createElement('div');
+bookDescription.className = 'book-description';
+// bookDescription.style.border = '1px solid red'
+detailsContainer.appendChild(bookDescription);
+
+export const bookTrama = document.createElement('p');
+bookTrama.className = 'book-trama';
+bookTrama.style.border = '1px solid black'
+bookDescription.appendChild(bookTrama);
