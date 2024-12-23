ea17b6a73e4612be9cfc6ff3a50fe8be81401766
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2024-12-23 12:26:28 +0100

    Funzione getBook ottimizzata

--- src/js/details.js
+++ src/js/details.js
@@ -1,10 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/style.scss';
+ const createDomElement = (tag, className, content) => {
+    const el = document.createElement(tag);
+    className.forEach(e => el.classList.add(e));
+    el.innerHTML = content;
+    return el;
+}
 
+export const detailsPage = createDomElement('div', ['card', 'details-page'], '');
+
+export const closeButton = createDomElement('button', ['btn', 'btn-outline-light', 'close-button'], 'X');
 
 //books' details container
-export const detailsContainer = document.createElement('div');
+ const detailsContainer = document.createElement('div');
 detailsContainer.className = 'details-container';
+detailsPage.append(closeButton, detailsContainer);
 
 //img's cover book
 export const imgCover = document.createElement('img');
@@ -19,5 +29,4 @@ export const detailsAuhtor = document.createElement('p');
 export const bookTrama = document.createElement('div');
 bookTrama.className = ' card-text book-trama';
 detailsContainer.append(imgCover,title,detailsAuhtor, bookTrama);
-
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/style.scss'
 import axios from 'axios';
-import { detailsContainer,title, detailsAuhtor, bookTrama, imgCover } from './details.js';
+import { detailsPage, closeButton, title, detailsAuhtor, bookTrama, imgCover  } from './details.js';
 
-const createDomElement = (tag, className, content) => {
+export const createDomElement = (tag, className, content) => {
     const el = document.createElement(tag);
     className.forEach(e => el.classList.add(e));
     el.innerHTML = content;
@@ -18,13 +18,12 @@ favicon.href = '../src/img/favicon.png';
 document.head.appendChild(favicon)
 
 //home page
-
 const homePage = createDomElement('div', ['homePage-container', 'container-fluid'], ' ');
 document.body.appendChild(homePage);
 
 //home page title
-
 const homePageTitle = createDomElement('h1', ['h1','homePage-title', ], 'Wonderful Books');
+
 //search bar
 const homePageSearchBar = createDomElement('input', ['homePage-searchBar'], '');
 homePageSearchBar.placeholder = 'Which story you want to know...';
@@ -35,7 +34,6 @@ const homePageButton = createDomElement('button', ['btn', 'btn-outline-success',
 const loading = createDomElement('div', ['spinner-border', 'text-success', 'loading-spinner'], ' ');
 homePage.append(homePageTitle, homePageSearchBar, homePageButton, loading)
 
-
 //books' cover container
 const coverBooksContainer = createDomElement('div', ['container', 'text-center', 'coverBooks-container'], ' ');
 document.body.appendChild(coverBooksContainer);
@@ -44,89 +42,103 @@ document.body.appendChild(coverBooksContainer);
 
 
 //create card
-
 function createCard(){
     const card = createDomElement('div', [ 'card'], '');
     coverBooksContainer.append(card);
     return card
 }
+// Book element constructor
+class Book {
+  constructor(bookData){
+      this.cover = bookData.cover;
+      this.title = bookData.title;
+      this.author = bookData.author;
+      this.key = bookData.key;
 
-//call of books
+
+      this.card = createCard();
+      this.coverBookImg = createDomElement('img', ['card-img-top', 'coverBook-img'], ' ');
+      this.cardBody = createDomElement('div', ['card-body'], ' ');
+      this.bookTitle = createDomElement('h3', ['card-title', 'book-title'], this.title);
+      this.bookAuthor = createDomElement('p', ['card-text', 'book-author'], this.author.slice(0, 3).join(', '));
+      this.detailsButton = createDomElement('button', ['btn', 'btn-outline-success', 'details-button'], ' Details...');
+  
+
+  }
+
+  createCardElements(){
+      this.coverBookImg.src = this.cover;
+      this.card.append(this.coverBookImg, this.cardBody);
+      this.cardBody.append(this.bookTitle, this.bookAuthor,this.detailsButton)
+      return this.card;
+
+  }
+  detailsBookEvent(getTramaBook, detailsPage, imgCover, title, detailsAuthor, bookTrama, closeButton){
+    this.detailsButton.addEventListener('click', async () =>{
+      detailsPage.style.display = 'block';
+      imgCover.src = this.cover;
+      title.textContent = this.title;
+      detailsAuthor.textContent = this.author;
+
+      try{
+          const description = await getTramaBook(this.key);
+          bookTrama.textContent = `${description}`
+
+      }catch(error){
+          alert('Description is not available')
+      };
+
+      closeButton.addEventListener('click', () => {
+          detailsPage.style.display = 'none';
+      })
+      document.body.append(detailsPage);
+    })
+  }
+  
+}
+
+
+
+
+//POI FARE LA FETCH API
 async function getBooks(){
-    let searchBooks = homePageSearchBar.value;
-    coverBooksContainer.innerHTML = '';
+  let serachBooks = homePageSearchBar.value;
+  coverBooksContainer.innerHTML = '';
 
 
-    try{
-        const response = await axios.get(`https://openLibrary.org/search.json?q=${searchBooks}&limit=20`);
-        const data = response.data;
+  try{
+      const response = await axios.get(`https://openLibrary.org/search.json?q=${serachBooks}&limit=20`);;
+      const data = response.data;
 
-        const books = data.docs.map(book => ({
-            cover: book.cover_i ? `https://covers.openLibrary.org/b/id/${book.cover_i}-M.jpg` : null,
-            title: book.title ,
-            author: book.author_name || [],
-            key: book.key
-        })).filter(book => book.cover && book.title && book.author.length > 0 );
-        
-
-        books.forEach(book =>{
-            const card = createCard();
-            loading.style.display = 'none';
-            coverBooksContainer.style.border= '1px solid #00853E';
-           
-            //books'cover
-            const coverBooksImg = createDomElement('img', ['card-img-top', 'coverBook-img'], ' ');
-            coverBooksImg.src = book.cover;
-
-            //title and author container
-            const cardBody = createDomElement('div', ['card-body'], ' ');
-            card.append(coverBooksImg, cardBody);
-
-            //book's title
-             const bookTitle = createDomElement('h3', ['card-title', 'book-title'], book.title);
-
-            //book's author
-             const bookAuthor = createDomElement('p', ['card-text', 'book-author'], book.author.slice(0, 3).join(', '));
-
-            //details' button
-            const detailsButton = createDomElement('button', ['btn', 'btn-outline-success', 'details-button'], ' Details...');
-            cardBody.append(bookTitle, bookAuthor, detailsButton)
-            
-            //details' book call
-            detailsButton.addEventListener('click',async () =>{
-            const detailsPage = createDomElement('div', ['card', 'details-page'], ' ');
-             const closeButton = createDomElement('button', ['btn', 'btn-outline-light', 'close-button'], 'X');
-             detailsPage.append(closeButton, detailsContainer );
-
-             imgCover.src = book.cover;
-             title.textContent = book.title;
-             detailsAuhtor.textContent = book.author;
-
-             try{
-               const description = await getTramaBook(book.key);
-               bookTrama.textContent = ` ${description}`;
-             }catch(e){
-                alert('Non sta funzionando');
-             } ;
-
-             closeButton.addEventListener('click', () =>{
-                detailsPage.style.display = 'none';
-              })
-             document.body.append(detailsPage);
-            })
-          
-
-        })
+      const books = data.docs.map(book =>({
+          cover: book.cover_i ? `https://covers.openLibrary.org/b/id/${book.cover_i}-M.jpg` : null,
+          title: book.title ,
+          author: book.author_name || [],
+          key: book.key
+      })).filter(book => book.cover && book.title && book.author.length > 0 );
+      
 
 
-    }catch(error){console.log(error)}
+      books.forEach(bookData =>{
+          const book = new Book(bookData);
+          const card = book.createCardElements();
+          coverBooksContainer.append(card)
+         
+
+        book.detailsBookEvent(getTramaBook,detailsPage, imgCover, title, detailsAuhtor, bookTrama, closeButton);
+         loading.style.display = 'none';
+         coverBooksContainer.style.border= '1px solid #00853E';
+      })
+
+  }catch(error){console.log(error)}
 };
 
 
-//var alert = document.getElementById('alert');
+
 const alert = createDomElement('div', ['alert', 'alert-danger'], 'Please enter a valid search term!');
 homePage.append(alert);
 alert.style.display = 'none';
+
 //alert and books' call 
 homePageButton.addEventListener('click', () =>{
   alertMessage()
@@ -145,11 +157,6 @@ function alertMessage (){
   }
 }
 
-
-
-
-
-      
 
 //trama's books call
 async function getTramaBook(bookKey){
